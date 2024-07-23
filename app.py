552187862098c76cbf3fbacd539eb22bd25a5c40
keiredin <se.keiredin.aman@gmail.com>
2024-07-23 20:27:54 +0300

    edit and delete task implemented

--- app.py
+++ app.py
@@ -1,12 +1,66 @@
-from flask import Flask, render_template
+from datetime import datetime
+from flask import Flask, redirect, render_template, request
 from flask_scss import Scss
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+Scss(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
 
-@app.route('/')
-def hello_world():
-    return render_template('index.html')
 
+class MyTask(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(100), nullable=False)
+    complete = db.Column(db.Integer, default=0)
+    created = db.Column(db.DateTime, default=datetime.now)
+
+    def __repr__(self):
+        return '<Task %r>' % self.id
+
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    if request.method == 'POST':
+        task_content = request.form['content']
+        new_task = MyTask(content=task_content)
+
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except Exception as e:
+            print(f'Error: {e}')
+            return f'Error: {e}'
+    else:
+        tasks = MyTask.query.order_by(MyTask.created).all()
+        return render_template('index.html', tasks=tasks)
+    
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    task_to_delete = MyTask.query.get_or_404(id)
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect('/')
+    except Exception as e:
+        print(f'Error: {e}')
+        return f'Error: {e}'
+    
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+def update(id):
+    task = MyTask.query.get_or_404(id)
+    if request.method == 'POST':
+        task.content = request.form['content']
+        try:
+            db.session.commit()
+            return redirect('/')
+        except Exception as e:
+            print(f'Error: {e}')
+            return f'Error: {e}'
+    else:
+        return render_template('update.html', task=task)
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
